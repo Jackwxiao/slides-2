f510775380aa2619ce60d3e836074fc53ab0ad3e
wuxiao <1291383707@qq.com>
2019-08-18 12:16:15 +0800

    苹果风格轮播初实现 echo "# slides-2" >> README.md git init git add README.md git commit -m "first commit" git remote add origin git@github.com:WUXIAOd/slides-2.git git push -u origin master

---
+++ main.js
@@ -0,0 +1,27 @@
+let $buttons = $('#buttonWrapper> button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
+
+
+bindEvents()
+
+
+function bindEvents(){
+    $('#buttonWrapper').on('click','button',function(e){
+       let $button = $(e.currentTarget)
+       let index = $button.index()
+       goToSlide(index)
+    })
+}
+
+function goToSlide(index){
+if(index>$buttons.length-1){
+    index=0
+}else{
+    $slides.css({
+        transform:`translateX(${-(index)*400}px)`
+    })
+}
+current = index
+}
