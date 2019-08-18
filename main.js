ad47eeaad1eff347c0f77b4e9093b21898aea006
wuxiao <1291383707@qq.com>
2019-08-18 12:27:07 +0800

    苹果风格轮播初实现

--- main.js
+++ main.js
@@ -7,21 +7,19 @@ let current = 0
 bindEvents()
 
 
-function bindEvents(){
-    $('#buttonWrapper').on('click','button',function(e){
-       let $button = $(e.currentTarget)
-       let index = $button.index()
-       goToSlide(index)
+function bindEvents() {
+    $('#buttonWrapper').on('click', 'button', function (e) {
+        let $button = $(e.currentTarget)
+        let index = $button.index()
+        goToSlide(index)
     })
 }
 
-function goToSlide(index){
-if(index>$buttons.length-1){
-    index=0
-}else{
-    $slides.css({
-        transform:`translateX(${-(index)*400}px)`
-    })
-}
-current = index
+function goToSlide(index) {
+    if (index > $buttons.length - 1) {
+        index = 0
+    } else {
+        $slides.css({ transform: `translateX(${-(index) * 400}px)` })
+    }
+    current = index
 }
